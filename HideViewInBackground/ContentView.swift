19caf079098e5b2843f373a762b35a6fb35efc3c
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-10 19:49:14 -0500

    privacy sensitive

--- HideViewInBackground/ContentView.swift
+++ HideViewInBackground/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world! 👋👋")
+            Text("This is sentence information")
+                .font(.title)
+                .privacySensitive()
         }
         .padding()
     }
