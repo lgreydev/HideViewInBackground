6ac099c6e3967f4c2cc32d36669249a48e572c8d
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-10 19:39:16 -0500

    Initial Commit

---
+++ HideViewInBackground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HideViewInBackground
+//
+//  Created by Serhii Lukashchuk on 2025-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HideViewInBackground/HideViewInBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideViewInBackgroundApp.swift
+//  HideViewInBackground
+//
+//  Created by Serhii Lukashchuk on 2025-01-10.
+//
+
+import SwiftUI
+
+@main
+struct HideViewInBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
